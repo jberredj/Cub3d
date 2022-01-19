21ffadf7f32f4ab7722414757043708a2560d021
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2022-01-19 16:48:31 +0100

    Correct error when any of resolution value is 0

--- srcs/parser/res_parser.c
+++ srcs/parser/res_parser.c
@@ -6,7 +6,7 @@
 /*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/26 13:36:27 by jberredj          #+#    #+#             */
-/*   Updated: 2021/12/26 13:52:28 by jberredj         ###   ########.fr       */
+/*   Updated: 2022/01/19 16:45:40 by jberredj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int	res_parser(int *width, int *height, bool *loaded, char **line)
 		return (INCORRECT_VALUE);
 	*width = ft_atoi(line[1]);
 	*height = ft_atoi(line[2]);
-	if (*height < 0 || *width < 0)
+	if (*height <= 0 || *width <= 0)
 		return (OUT_OF_RANGE);
 	*loaded = true;
 	return (0);
