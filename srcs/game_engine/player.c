cb038e8a1138655a11519f9cce5e74cc56e39333
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2022-01-20 11:10:02 +0100

    Protect out of band reaching

--- srcs/game_engine/player.c
+++ srcs/game_engine/player.c
@@ -6,7 +6,7 @@
 /*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/26 16:39:46 by jberredj          #+#    #+#             */
-/*   Updated: 2021/12/26 19:13:16 by jberredj         ###   ########.fr       */
+/*   Updated: 2022/01/20 11:08:49 by jberredj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,11 +47,11 @@ static void	player_straff(t_player *player, t_map map)
 		movestep = player->movespeed;
 	new_x = player->x + cos(player->straf_dir + player->rot_angl) * movestep;
 	new_y = player->y + sin(player->straf_dir + player->rot_angl) * movestep;
-	if (((int)(new_x) < map.x && (int)(new_x) >= 0
-		&& map.grid[(int)(new_x)][(int)(player->y)] != '1') || !BONUS)
+	if ((int)(new_x) < map.x - 1 && (int)(new_x) > 0
+		&& (map.grid[(int)(new_x)][(int)(player->y)] != '1' || !BONUS))
 		player->x = new_x;
-	if (((int)(new_y) < map.y && (int)(new_y) >= 0 &&
-		map.grid[(int)(player->x)][(int)(new_y)] != '1') || !BONUS)
+	if ((int)(new_y) < map.y - 1 && (int)(new_y) > 0 &&
+		(map.grid[(int)(player->x)][(int)(new_y)] != '1' || !BONUS))
 		player->y = new_y;
 }
 
@@ -67,11 +67,11 @@ void	player_pos(t_player *player, t_map map, int mouse_x_vel)
 	movestep = player->walk_dir * player->movespeed;
 	new_x = player->x + cos(player->rot_angl) * movestep;
 	new_y = player->y + sin(player->rot_angl) * movestep;
-	if (((int)(new_x) < map.x && (int)(new_x) >= 0
-		&& map.grid[(int)(new_x)][(int)(player->y)] != '1') || !BONUS)
+	if ((int)(new_x) < map.x - 1 && (int)(new_x) > 0
+		&& (map.grid[(int)(new_x)][(int)(player->y)] != '1' || !BONUS))
 		player->x = new_x;
-	if (((int)(new_y) < map.y && (int)(new_y) >= 0
-		&& map.grid[(int)(player->x)][(int)(new_y)] != '1') || !BONUS)
+	if ((int)(new_y) < map.y - 1 && (int)(new_y) > 0
+		&& (map.grid[(int)(player->x)][(int)(new_y)] != '1' || !BONUS))
 		player->y = new_y;
 	player_straff(player, map);
 }
